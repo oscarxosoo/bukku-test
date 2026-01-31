db8716b38cac5965c24faf0d13f027ea937889e3
oscar-soo <oscar@indochat.co.id>
2026-01-31 22:34:21 +0800

    - added migration

---
+++ database/migrations/2026_01_31_221517_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->enum('transaction_type', ['purchase', 'sale']);
+            $table->date('transaction_date')->unique();
+            $table->unsignedInteger('quantity');
+            $table->decimal('unit_price', 15, 2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ database/migrations/2026_01_31_221552_create_inventory_ledger_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_ledger', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->unique()->constrained('transactions');
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->unsignedInteger('quantity_on_hand');
+            $table->decimal('average_cost_per_unit', 15, 4);
+            $table->decimal('total_inventory_value', 15, 2);
+            $table->decimal('cost_of_goods_sold', 15, 2)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_ledger');
+    }
+};
